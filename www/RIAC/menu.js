a5b002f016f070b717ddabc65d90357af2942f26
shadowlerone <andrae.lewis@gmail.com>
2022-02-12 15:45:11 -0500

    made menu classes made top bar added menu code

--- www/RIAC/menu.js
+++ www/RIAC/menu.js
@@ -1,13 +1,36 @@
-function openMenu() {
-	document.getElementById("menu-container").classList.add("show");
+function openMenu(id) {
+	document.getElementById(id).classList.add("show");
 	console.info("Menu Opened");
 }
 
-function closeMenu() {
-	document.getElementById("menu-container").classList.remove("show");
+function closeMenu(id) {
+	document.getElementById(id).classList.remove("show");
 	console.info("Menu Closed");
 }
 
 function testButton() {
 	console.debug("button pressed");
 }
+
+// function closeMenu(this) {
+// 	if (this.classList.contains('menu')) {
+// 		this.parent.classList.remove("show");
+// 	}
+// }
+
+Array.from(document.getElementsByClassName("menu-toggle")).forEach(e => {
+	e.addEventListener('click', (event) => {
+		console.log(event.target.dataset.menuTarget + '-menu-container')
+		// document.getElementById(event.target.dataset.menuTarget + '-container').classList.add("show");
+		openMenu(event.target.dataset.menuTarget + '-menu-container');
+	})
+})
+
+Array.from(document.getElementsByClassName("menu")).forEach(e => {
+	e.addEventListener('click', (event) => {
+		event.target.parentElement.classList.remove("show");
+		if (event.target.dataset.parentMenu) {
+			openMenu(event.target.dataset.parentMenu + '-menu-container')
+		}
+	})
+})
