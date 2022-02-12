0a446c0ec29c565fe9ddc90b5b9d86ef0cba5b48
shadowlerone <andrae.lewis@gmail.com>
2022-02-12 16:58:20 -0500

    Local sample project complete

--- www/RIAC/menu.js
+++ www/RIAC/menu.js
@@ -3,20 +3,31 @@ function openMenu(id) {
 	console.info("Menu Opened");
 }
 
+function closeMenus() {
+	Array.from (document.getElementsByClassName("menu-container")).forEach (e => e.classList.remove("show"))
+}
+
 function closeMenu(id) {
 	document.getElementById(id).classList.remove("show");
 	console.info("Menu Closed");
 }
+function loadList(url) {
+	const xhttp = new XMLHttpRequest();
+	xhttp.onload = function () {
+		
+		// loadFromString(this.responseText)
+		// document.getElementById("demo").innerHTML = this.responseText;
+	}
+	xhttp.open("GET", url, true);
+	xhttp.send();
+}
+
 
 function testButton() {
 	console.debug("button pressed");
 }
 
-// function closeMenu(this) {
-// 	if (this.classList.contains('menu')) {
-// 		this.parent.classList.remove("show");
-// 	}
-// }
+// Menu toggle code
 
 Array.from(document.getElementsByClassName("menu-toggle")).forEach(e => {
 	e.addEventListener('click', (event) => {
@@ -26,6 +37,8 @@ Array.from(document.getElementsByClassName("menu-toggle")).forEach(e => {
 	})
 })
 
+// Menu closing
+
 Array.from(document.getElementsByClassName("menu")).forEach(e => {
 	e.addEventListener('click', (event) => {
 		event.target.parentElement.classList.remove("show");
@@ -34,3 +47,27 @@ Array.from(document.getElementsByClassName("menu")).forEach(e => {
 		}
 	})
 })
+
+// Dynamic Menus, these send API calls to create their lists
+
+Array.from(
+	document.getElementsByClassName("dynamic-menu"))
+	.forEach(e => {
+		fetch(e.dataset.listUrl)
+			.then (response => response.json())
+			.then (data => data.projects
+				.forEach(
+					(p) => {
+						b = document.createElement('div');
+						b.classList.add("button");
+						b.addEventListener('click', () => {
+							loadFromURL(p.filepath);
+							closeMenus();
+						});
+						b.textContent = p.name;
+						e.appendChild(b);
+						// console.log(e);
+					}
+				))
+	})
+
